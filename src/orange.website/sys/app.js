bdfd9753a283bd4397f4fe0df207dee3b32ae340
15001217168 <1076239475@qq.com>
2017-01-23 23:27:13 +0800

    添加验证

--- src/orange.website/sys/app.js
+++ src/orange.website/sys/app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport=require('passport'),LocalStrategy=require('passport-local').Strategy;
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
 
-var router = require('./router');
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
 
-var app = express();
+    router = require('./router'),
+    app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,30 +22,33 @@ app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(cookieParser());
+app.use(session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	console.log(err);
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error/500');
+    console.log(err);
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error/500');
 });
 
 module.exports = app;
