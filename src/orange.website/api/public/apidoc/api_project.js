337ce327b077db6b2bd4b3bafc4b5741c8b413f0
刘雪松 <liuxuesong@zccninfo.com>
2017-02-15 18:16:11 +0800

    af

---
+++ src/orange.website/api/public/apidoc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "orange",
+  "version": "0.1.0",
+  "description": "orange",
+  "template": {
+    "forceLanguage": "zh-cn"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-02-15T08:01:30.312Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
--- src/orange.website/api/router.js
+++ src/orange.website/api/router.js
@@ -4,8 +4,39 @@ var express = require('express'),
 	oauth2 = require('../../orange.middleware/oauth2'),
 	smsUtil = require('../../orange/sms/top_client'),
 	sysUserService = require('../../orange.service/sys_user_service');
-
+/**
+ * @api {post} /authorize/:appid
+ * @apiVersion 0.3.0
+ * @apiName 获取授权Token
+ * @apiDescription 获取授权Token
+ *
+ * @apiParam {String} appid 客户端ip
+ *
+ * @apiExample Example usage:
+ * curl -i http://localhost/user/4711
+ *
+ * @apiSuccess {String}   id            The Users-ID.
+ * @apiSuccess {Date}     registered    Registration Date.
+ * @apiSuccess {Date}     name          Fullname of the User.
+ * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
+ * @apiSuccess {Object}   profile       Profile data (example for an Object)
+ * @apiSuccess {Number}   profile.age   Users age.
+ * @apiSuccess {String}   profile.image Avatar-Image.
+ * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
+ * @apiSuccess {String}   options.name  Option Name.
+ * @apiSuccess {String}   options.value Option Value.
+ *
+ * @apiError NoAccessRight Only authenticated Admins can access the data.
+ * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
 router.post('/authorize', oauth2.access_token);
+
 router.post('*', oauth2.authorization);
 router.post('/api/send_sms_code', function (req, res, next) {
 	var phone = req.body.phone;
